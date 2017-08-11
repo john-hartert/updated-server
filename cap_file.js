2b081bb0571bf043f8d6765c76b344168bcc11c5
John Hartert <j.blainehartert@gmail.com>
2017-08-10 23:35:44 -0400

    Updates server work

--- cap_file.js
+++ cap_file.js
@@ -1,14 +1,36 @@
+// const readline = require('readline');
+// const fs = require('fs');
+// const outputFileSync = require('output-file-sync');
+
+// const rl = readline.createInterface({
+//   input: fs.createReadStream('file1.txt')
+// });
+
+// rl.on('line', (line) => {
+//   outputFileSync('output.txt', line);
+// });
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 const readline = require('readline');
 const fs = require('fs');
 const outputFileSync = require('output-file-sync');
 
+
 const rl = readline.createInterface({
-  input: fs.createReadStream('file1.txt')
+    input: process.stdin,
+    output: process.stdout
 });
 
-rl.on('line', (line) => {
-  outputFileSync('output.txt', line);
+rl.question('Enter a file: ', (value) => {
+    let domain = value
+    rl.close();
+    fs.createReadStream(domain)
+    rl.question('Enter a new file: ', (valueOne) => {
+        let domainOne = valueOne
+        rl.on('line', (line) => {
+        outputFileSync(domainOne, line);
+        rl.close();
+    });
 });
 
-// const fs = require('fs');
-// outputFileSync('output.txt', line);
+});
